d4b358295920d797fba8741d29145ba4e1670aec
Oskar Størmer <oskar.stormer@gmail.com>
2020-10-20 17:39:08 +0200

    fix: Updated plot date formatting

--- prestudy.py
+++ prestudy.py
@@ -1,5 +1,9 @@
 import pandas as pd
+from datetime import datetime
 from matplotlib import pyplot as plt
+from matplotlib.dates import date2num
+plt.close("all")
+
 
 fullDataSet = pd.read_csv("data/owid-covid-data_20_10_2020.csv")
 # Select what columns we are interested in
@@ -24,9 +28,13 @@ euroData.to_csv("data/euro_countries.csv")
 for isoCode in euroCountryCodes:
     countryData = euroData[euroData["iso_code"] == isoCode]
     countryName = (countryData.location.unique()[0])
-    plt.plot(countryData["total_cases"])
-    plt.plot(countryData["new_cases_smoothed"])
+    dateTimes = [datetime.strptime(date, "%Y-%m-%d") for date in countryData["date"]]
+    dates = date2num(dateTimes)
+
+    plt.plot_date(dates, countryData["total_cases"], "-")
+    plt.plot_date(dates, countryData["new_cases_smoothed"], "-")
+    plt.xticks(rotation=30, horizontalalignment="right")
     plt.title(countryName)
-    plt.legend()
+    plt.legend(["Total cases", "New cases (7 days smoothed)"])
     plt.savefig("plots/{:s}.png".format(isoCode))
     plt.clf()
